93d0998d816496a60aa3eccb1025b397c3df82bf
frakay100 <frakay100@users.noreply.github.com>
2021-05-01 12:42:29 +0100

    Update step-track.js

--- step-track.js
+++ step-track.js
@@ -1,22 +1,49 @@
 $(document).ready(function() {
+  
+  /** FYI: THE SCRIPT USES THE RAYDION 60 SECOND TIMER. IF THIS STOPS THE SCRIPT WILL PAUSE **/
+  /** FILTER BY REWARDS TO CLEAN RAYDIUM NOISE FROM CONSOLE **/
 
-    setInterval(function() {
+  let v = {}, outputs = [], isTimed;
+  let startTime = new Date().getTime();
+  
+  setInterval(function() {
 
-        let previousValue = parseFloat(sessionStorage.getItem('step'));
-        let currentValue = parseFloat($('.token').last().text());
+        $('.value').each(function(i, obj) {
+                
+                let symbol = obj.innerText.replace(/[^a-zA-Z]/g,'').replace(/[NaN]/g,'');;
+                
+                if (parseFloat(obj.innerText) > 0 && symbol && symbol.length > 3) {
+                    
+                    v[symbol] = v[symbol] || [0,0,0]; // current, previous, difference
+   
+                    isTimed = (symbol === 'COPE');
+                    
+                    // raydium page value
+                    let current = parseFloat(obj.innerText);
 
-        sessionStorage.setItem('step', currentValue + '');
+                    v[symbol][1] = (current > v[symbol][0]) ? v[symbol][0] : (v[symbol][1])
+                    v[symbol][0] = current;
+                    v[symbol][2]= (v[symbol][0] - v[symbol][1]);
 
-        if (previousValue && previousValue < currentValue) {
+                    if (v[symbol][2] > 0) { 
+                        for (key in v) {
 
-                let tokens = currentValue - previousValue;
-                console.log('STEP: Tokens per Minute = ' + tokens.toFixed(2));
-                localStorage.setItem('lastMessage', 'STEP: Tokens per Minute = ' + tokens.toFixed(2))
-    
+                                let output  = 'REWARDS : ' + key + ' Per Minute = ' + ((v[symbol][2] === current) ? 'Waiting...' : v[symbol][2])
 
-        }
+                                if (outputs.indexOf(output) === -1) {
+                                    console.log(output)
+                                    outputs.push(output)
+                                } else {
+                                    outputs[outputs.indexOf(output)] = output;
+                                }
 
+                              
+                            }
+                        }
+                     }
+                
+                })
 
-    }, 1000)
+   }, 1000)
 
-})
+});
